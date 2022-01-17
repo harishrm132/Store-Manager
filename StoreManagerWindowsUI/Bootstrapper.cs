3cc939fa4775f532eb58162f1592cb3d62caad5c
Harish <harishrm132@gmail.com>
2022-01-17 20:58:29 +0530

    📦️ NEW :  Created WPF Project

---
+++ StoreManagerWindowsUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using StoreManagerWindowsUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StoreManagerWindowsUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
