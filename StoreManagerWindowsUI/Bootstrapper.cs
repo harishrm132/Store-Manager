04c9a805ab07d3f3dc15c12c3a632a493be48a7e
Harish <harishrm132@gmail.com>
2022-01-17 23:49:22 +0530

    📦️ NEW : Added Dependency Injection(DI) system
    - Use Caliburn micro for MVVM framework. Also includes simple container DI system.

--- StoreManagerWindowsUI/Bootstrapper.cs
+++ StoreManagerWindowsUI/Bootstrapper.cs
@@ -11,14 +11,48 @@ namespace StoreManagerWindowsUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            //wire viewmodel that connect to views - reflection
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(vmType => _container.RegisterPerRequest(vmType, vmType.ToString(), vmType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
