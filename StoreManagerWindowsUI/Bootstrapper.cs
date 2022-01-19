2fc27505ead92d26bfcd664ad7727230e5d02669
Harish <harishrm132@gmail.com>
2022-01-19 23:02:25 +0530

    📦️ NEW :  Login Form Created
    - UI & Property Mapped

--- StoreManagerWindowsUI/Bootstrapper.cs
+++ StoreManagerWindowsUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using StoreManagerWindowsUI.Helpers;
 using StoreManagerWindowsUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace StoreManagerWindowsUI
 {
@@ -17,6 +19,11 @@ namespace StoreManagerWindowsUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
         protected override void Configure()
---
+++ StoreManagerWindowsUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StoreManagerWindowsUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public bool CanLogin
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password))
+                {
+                    return true;
+                }
+                return false; 
+            }
+        }
+        
+        public void Login()
+        {
+        }
+
+    }
+}
